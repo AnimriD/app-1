125068be16b6b307909b369f2425ee6dd9ee7bf8
AnimriD <144551077+AnimriD@users.noreply.github.com>
2024-04-27 12:28:07 -0400

    Add files via upload

--- Hello.py
+++ Hello.py
@@ -1,7 +1,18 @@
 import streamlit as st
-import matplotlib as plt
+import matplotlib.animation as mpla
+import matplotlib.pyplot as plt
 import numpy as np
 
+T=np.linspace(0, 2*np.pi, 100)
+S= np.sin(T)
+line, = plt.plot(T, S)
+
+def animate(i):
+    line.set_ydata(np.sin(T+ i/50))
+    
+anim = mpla.FuncAnimation(plt.gcf(), animate, interval =5)
+plt.show()
+                   
 def sierpinski(x, y, length, depth):
     if depth == 0:
         return
