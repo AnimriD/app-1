bf6e77fbbf4e886c07f3489174cc7a7d48d6cef4
AnimriD <144551077+AnimriD@users.noreply.github.com>
2024-04-27 20:12:33 -0400

    Add files via upload

--- Hello.py
+++ Hello.py
@@ -1,54 +1,41 @@
 import streamlit as st
-import turtle
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
 import random
 
-def modify_sierpinski(t, depth, length):
+def modify_sierpinski(ax, t, depth, length):
     if depth == 1:
-        t.lt(60)
-        t.fd(length)
-        t.rt(120)
-        t.fd(length)
-        t.rt(120)
-        t.fd(length)
-        t.rt(180)
+        t.set_data([t.get_xdata(), [t.get_ydata()[0], t.get_ydata()[0] + length]])
     else:
-        modify_sierpinski(t, depth - 1, length/2)
-        t.fd(length/2)
-        t.color(random.randrange(256), random.randrange(256), random.randrange(256))
-        modify_sierpinski(t, depth - 1, length / 2)
-        t.color(random.randrange(256), random.randrange(256), random.randrange(256))
-        t.bk(length / 2)
-        t.lt(60)
-        t.fd(length / 2)
-        t.rt(60)
-        modify_sierpinski(t, depth - 1, length / 2)
-        t.color(random.randrange(256), random.randrange(256), random.randrange(256))
-        t.rt(120)
-        t.fd(length / 2)
-        t.lt(120)
+        modify_sierpinski(ax, t, depth - 1, length / 2)
+        ax.draw_artist(t)
+        ax.figure.canvas.draw()
+        t.set_ydata([t.get_ydata()[0], t.get_ydata()[0] + length])
+        t.set_xdata([t.get_xdata()[0] + length / 2, t.get_xdata()[1]])
+        ax.draw_artist(t)
+        ax.figure.canvas.draw()
+        modify_sierpinski(ax, t, depth - 1, length / 2)
+        ax.draw_artist(t)
+        ax.figure.canvas.draw()
+        t.set_ydata([t.get_ydata()[0], t.get_ydata()[0]])
+        t.set_xdata([t.get_xdata()[0] - length / 4, t.get_xdata()[1] - length / 2])
+        ax.draw_artist(t)
+        ax.figure.canvas.draw()
+        modify_sierpinski(ax, t, depth - 1, length / 2)
+        ax.draw_artist(t)
+        ax.figure.canvas.draw()
 
-def draw_sierpinski(depth, length):
-    t = turtle.Turtle()
-    screen = turtle.Screen()
-    screen.tracer(0, 0)  # Disable automatic screen updates
-    t.speed(0)  # Set maximum turtle speed
-    t.penup()
-    t.goto(-length / 2, -length / 2)  # Center the triangle
-    t.pendown()
-    modify_sierpinski(t, depth, length)
-    screen.update()  # Update the screen once drawing is complete
-    screen.mainloop()  # Keep the window open until manually closed
+def animate(i):
+    modify_sierpinski(ax, line, i, 300)
+    return line,
 
 def main():
-    st.title('Sierpinski Triangle Generator')
-    st.sidebar.header('Parameters')
-    depth = st.sidebar.slider('Depth', min_value=1, max_value=7, value=3)
-    length = st.sidebar.slider('Length', min_value=50, max_value=500, value=300)
-    st.write('Adjust the parameters in the sidebar to change the Sierpinski triangle.')
-
-    if st.button('Draw Sierpinski Triangle'):
-        st.write('Drawing Sierpinski Triangle...')
-        draw_sierpinski(depth, length)
+    fig, ax = plt.subplots()
+    ax.set_xlim([-150, 150])
+    ax.set_ylim([0, 260])
+    line, = ax.plot([0, 0], [0, 0])
+    ani = FuncAnimation(fig, animate, frames=7, interval=1000, blit=True)
+    st.write(ani.to_jshtml())
 
 if __name__ == '__main__':
     main()
